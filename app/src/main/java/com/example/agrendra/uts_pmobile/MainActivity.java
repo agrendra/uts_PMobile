db36a366d86e91dab14a9a0f93278ba219ed7081
Ilyasdavid <ilyasds520@gmail.com>
2017-11-22 14:48:53 +0700

    custom list

--- app/src/main/java/com/example/agrendra/uts_pmobile/MainActivity.java
+++ app/src/main/java/com/example/agrendra/uts_pmobile/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.agrendra.uts_pmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
+    ListView list;
+    String[] itemname = {
+            "Rumus Luas dan Keliling",
+            "Rumus Luas dan Kelilingi",
+            "Rumus Luas dan Keliling",
+            "Rumus Luas dan Keliling",
+            "Rumus Luas dan Keliling",
+            "Rumus Luas dan Keliling"
+    };
+
+    Integer[] imgid = {
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon
+    };
+
+    String[] descriptionitem = {
+            "Persegi Panjang",
+            "Persegi",
+            "Lingkaran",
+            "Segitiga",
+            "Trapesium",
+            "Jajar Genjang"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
