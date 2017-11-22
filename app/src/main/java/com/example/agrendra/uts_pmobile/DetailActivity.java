36d61f0de44fcde046f24d520a8fec337fa5bd06
agrendra <agerf11@gmail.com>
2017-11-22 22:47:19 +0700

    Update beberapa file

--- app/src/main/java/com/example/agrendra/uts_pmobile/DetailActivity.java
+++ app/src/main/java/com/example/agrendra/uts_pmobile/DetailActivity.java
@@ -1,19 +1,22 @@
 package com.example.agrendra.uts_pmobile;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        //textView = (TextView)findViewById(R.id.)
         Intent intent = getIntent();
-
+        getSupportActionBar().setTitle(intent.getStringExtra("Tit"));
         if(getSupportActionBar()!=null){
             getSupportActionBar().setDisplayShowHomeEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
