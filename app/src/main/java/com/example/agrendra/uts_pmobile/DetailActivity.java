334b1b30c93ff0b1f9f4b1461318beab99641a5c
agrendra <agerf11@gmail.com>
2017-11-22 21:20:03 +0700

    Detail ke 1 Update

---
+++ app/src/main/java/com/example/agrendra/uts_pmobile/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.example.agrendra.uts_pmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
